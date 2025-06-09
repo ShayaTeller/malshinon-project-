3e1994f72a6678b599245c3bd7db1104411355d6
david teller <ydt009@gmail.com>
2025-06-09 11:44:41 +0300

    new clases

---
+++ malshinon/person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace malshinon
+{
+    internal class People
+    {
+        public int id;
+        public string firstName;
+        public string lastName;
+        public string secretCode;
+        public string potential_agent;
+        public int num_reports;
+        public int num_mentions;
+
+
+
+
+
+    }
+}
+
+ //('reporter', 'target', 'bot')  
